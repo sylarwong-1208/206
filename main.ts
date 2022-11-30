a5f8390f3c5fdbcddfa5c64bc1c81d2ed5046876
sylarwong-1208 <119169132+sylarwong-1208@users.noreply.github.com>
2022-12-01 00:24:19 +0800

    MakeCode 專案的初始檔案

---
+++ main.ts
@@ -0,0 +1,16 @@
+radio.onReceivedNumber(function (receivedNumber) {
+    basic.showNumber(receivedNumber)
+})
+input.onButtonPressed(Button.A, function () {
+    radio.sendString("abcdefg")
+})
+radio.onReceivedString(function (receivedString) {
+    basic.showString(receivedString)
+})
+input.onButtonPressed(Button.B, function () {
+    radio.sendString("bcdefgh")
+})
+radio.sendNumber(1)
+basic.forever(function () {
+	
+})
